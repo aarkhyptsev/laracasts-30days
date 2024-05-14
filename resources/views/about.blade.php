c154065c9e067cedd555909178a0e9b871093211
aarkhyptsev <aarkhyptsev@gmail.com>
2024-05-14 21:56:35 +0300

    day 6 finished

--- resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        About Page
-    </x-slot:heading>
-    <H1>Hello From the About Page</H1>
-</x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,5 @@
         Home Page
     </x-slot:heading>
     <H1>Hello From the Home Page</H1>
+
 </x-layout>
